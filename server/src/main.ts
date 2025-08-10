2277b1c68cdb18958f8d61d04294e8790132da69
Anjana Edirisinghe <anjanaape@gmail.com>
2025-08-10 16:58:25 +0530

    Refactor CORS configuration and improve static file serving in main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,20 +1,23 @@
 import { join } from 'path';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { static as expressStatic } from 'express';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  //CORS configuration
+  // CORS configuration
   app.enableCors({
     origin: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
     credentials: true,
   });
-  
-  app.use('/pdfs', expressStatic(join(__dirname, '..', 'pdfs')));
+
+  // Serve static files (PDFs)
+  app.use('/pdfs', express.static(join(__dirname, '..', 'pdfs')));
+
+  app.setGlobalPrefix('api');
 
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
